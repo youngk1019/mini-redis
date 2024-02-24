be3b64351695c41e05e8b9caf19e86bb1b37f578
youngk1019 <1186354278@qq.com>
2024-02-24 23:44:16 +0800

    handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{BufRead, Write};
-use std::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::io;
 
 struct Encoder;
 
@@ -9,23 +10,25 @@ impl Encoder {
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                loop {
-                    let mut reader = std::io::BufReader::new(&stream);
-                    let mut buf = vec![];
-                    match reader.read_until(b'\n', &mut buf) {
-                        Ok(0) => break,
-                        Ok(_) => stream.write_all(Encoder::encode_string("PONG").as_bytes()).unwrap(),
-                        Err(e) => panic!("{}", e),
-                    }
-                }
-            }
-            Err(e) => panic!("{}", e),
-        }
+#[tokio::main]
+async fn main() -> Result<(), io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let _ = connection(stream).await;
+        });
     }
+}
+
+async fn connection(stream: TcpStream) -> Result<(), io::Error> {
+    let mut stream = BufReader::new(stream);
+    let mut line = String::new();
+    while stream.read_line(&mut line).await? > 0 {
+        if line.to_ascii_uppercase().starts_with("PING") {
+            stream.write_all(Encoder::encode_string("PONG").as_bytes()).await?;
+        }
+        line.clear();
+    }
+    Ok(())
 }
