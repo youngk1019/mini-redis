7ba3180eaa8b9cb2016acf4072bcddcdc8c8fc45
youngk1019 <1186354278@qq.com>
2024-02-28 22:35:01 +0800

    receive handshake: psync

--- src/cmd/psync.rs
+++ src/cmd/psync.rs
@@ -1,5 +1,4 @@
 use async_trait::async_trait;
-use bytes::Bytes;
 use tokio::io::AsyncWriteExt;
 use crate::connection::{Applicable, Connection};
 use crate::encoder::Encoder;
@@ -9,18 +8,22 @@ use crate::resp::Type;
 #[derive(Debug, Default, PartialEq)]
 pub struct PSync {
     id: Option<String>,
-    offset: Option<i64>,
+    offset: Option<u64>,
 }
 
 impl TryFrom<&mut Parse> for PSync {
     type Error = crate::Error;
     fn try_from(parse: &mut Parse) -> crate::Result<Self> {
         let mut id = None;
+        let mut offset = None;
         let master_id = parse.next_string()?;
         if master_id.to_uppercase() != "?" {
             id = Some(master_id);
         }
-        let offset = Some(parse.next_int()? as i64);
+        match parse.next_int() {
+            Ok(o) => { offset = Some(o); }
+            _ => {}
+        }
         Ok(PSync { id, offset })
     }
 }
@@ -29,12 +32,8 @@ impl TryFrom<&mut Parse> for PSync {
 impl Applicable for PSync {
     async fn apply(self, dst: &mut Connection) -> crate::Result<()> {
         let role = dst.get_db().role();
-        let mut resp = vec![Type::SimpleString("FULLRESYNC".to_string())];
-        if let None = self.id {
-            resp.push(Type::BulkString(Bytes::from(role.id())));
-        }
-        resp.push(Type::BulkString(Bytes::from(role.offset().to_string())));
-        dst.write_all(Encoder::encode(&Type::Array(resp)).as_slice()).await?;
+        let resp = Type::SimpleString(format!("FULLRESYNC {} {}", role.id(), self.offset.unwrap_or(0)).into());
+        dst.write_all(Encoder::encode(&resp).as_slice()).await?;
         dst.flush().await?;
         Ok(())
     }
