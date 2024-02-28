a4ffa6dd1c72ee9b77b30393a92c6e51fd88a225
youngk1019 <1186354278@qq.com>
2024-02-28 15:29:39 +0800

    configure listening port

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use tokio::net::TcpListener;
-
+use clap::Parser;
 use redis_starter_rust::{listener, db};
+
+
+#[derive(Parser)]
+struct Config {
+    #[clap(long, default_value_t = 6379)]
+    port: usize,
+}
 
 
 #[tokio::main]
 async fn main() -> redis_starter_rust::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let cfg = Config::parse();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", cfg.port)).await?;
     let db = db::DB::new();
     listener::Listener::new(db, listener).run().await
 }
