eef220e0853841736918d8095067ebb317b78331
youngk1019 <1186354278@qq.com>
2024-03-08 16:53:03 +0800

    add help info

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use redis::{listener, db, replication};
 
 #[derive(Parser)]
 struct Config {
-    #[clap(long, default_value_t = 6379)]
+    #[clap(long, default_value_t = 6379, help = "Port to listen on")]
     port: usize,
 
-    #[clap(long = "replicaof", number_of_values = 2)]
+    #[clap(long = "replicaof", number_of_values = 2, help = "Replicate to master server [ip, port]")]
     replica: Option<Vec<String>>,
 
-    #[clap(long, default_value = ".")]
+    #[clap(long, default_value = ".", help = "RDB file directory")]
     dir: String,
 
-    #[clap(long, default_value = "dump.rdb")]
+    #[clap(long, default_value = "dump.rdb", help = "RDB file name")]
     dbfilename: String,
 }
 
